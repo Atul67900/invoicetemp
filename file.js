c96f3f881e390b0a8a7bce3be0395f3c199496d6
Atul Sharma <95357051+Atul67900@users.noreply.github.com>
2022-08-30 16:03:46 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,10 @@
+
+// console.log(contents.length);
+let contents = document.getElementById("contents");
+let srno = document.getElementById("srNo");
+if(srno.innerHTML == 9){
+    let top = document.getElementById("top");
+    top.style.height = "200vh"
+}else{
+    console.log("error");
+}
